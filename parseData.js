165117c36943c9e8ec1bdcec69d28d97a1663365
Nadav Green <nadavgreen@Nadavs-MacBook-Pro.local>
2019-07-22 23:14:54 -0400

    created functions to grab funciton name

--- parseData.js
+++ parseData.js
@@ -1,5 +1,27 @@
 const dataParser = (func) => {
+	const funcObj = {
+		name: '',
+		
+	};
+	funcObj['name'] = funcNameFinder(func)
 	console.log(func)
+	console.log(`The Big O notation of the function "${funcObj['name']}" is `)
+}
+
+const funcNameFinder = (arr) => {
+	let name = '';
+	for(let i = 0; i < arr.length; i++){
+		if(arr[i-1] === 'function') name = parenRemove(arr[i]);
+	}
+	return name;
+}
+
+const parenRemove = (str) => {
+	let newStr = '';
+	for(let i = 0; i < str.length - 2; i++){
+		newStr += str[i];
+	};
+	return newStr;
 }
 
 module.exports = dataParser;
