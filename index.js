6c2a768fef40b244459b03d9c47b7002de4504a9
Nadav Green <43800418+nadavgreen@users.noreply.github.com>
2019-07-26 16:53:59 -0400

    Logic (#7)
    * changed the header tags in the readme
    
    * created functions to grab funciton name
    
    * renamed index.js to main.js
    
    * renamed index.js to main.js also created an array with the for operators and closures but cant get the last closure

--- index.js
+++
@@ -1,9 +0,0 @@
-const myForLoop = () => {
-	let sum = 0
-	const arr = [1,2,3,4,5]
-	for(let i = 0; i < arr.length; i++){
-		for(let j = 0; j < arr.length; j++){
-			sum = sum + arr[i] + arr[j]
-		}
-	}
-}
